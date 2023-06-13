e830debed418c5132302e3bbfa767a4fa2b7a48b
TheLastFlame <thelastsight@mail.ru>
2023-06-13 20:09:55 +1000

    fix bug with vertical padding

--- lib/pages/home/widgets/tasklist.dart
+++ lib/pages/home/widgets/tasklist.dart
@@ -22,58 +22,53 @@ class TaskList extends StatelessWidget {
         padding: const EdgeInsets.all(appPadding),
         child: Card(
           elevation: 2,
-          child: Padding(
-            padding: controller.taskList.isNotEmpty
-                ? const EdgeInsets.symmetric(vertical: appPadding)
-                : EdgeInsets.zero,
-            child: Observer(builder: (_) {
-              var list = Computed(() => controller.taskList
-                  .where((el) =>
-                      controller.isComplitedVisible.value ||
-                      !el.isCompleted.value)
-                  .toList());
-              return Column(
-                children: [
-                  AnimatedSize(
-                    alignment: Alignment.topCenter,
-                    duration: animationsDuration,
-                    child: ListView.builder(
-                      padding: EdgeInsets.zero,
-                      shrinkWrap: true,
-                      physics: const NeverScrollableScrollPhysics(),
-                      itemCount: list.value.length,
-                      itemBuilder: (context, index) => Task(
-                        list.value[index],
-                        first: index == 0,
-                        last: index == list.value.length-1,
+          child: Observer(builder: (_) {
+            var list = Computed(() => controller.taskList
+                .where((el) =>
+                    controller.isComplitedVisible.value ||
+                    !el.isCompleted.value)
+                .toList());
+            return Column(
+              children: [
+                AnimatedSize(
+                  alignment: Alignment.topCenter,
+                  duration: animationsDuration,
+                  child: ListView.builder(
+                    padding: EdgeInsets.zero,
+                    shrinkWrap: true,
+                    physics: const NeverScrollableScrollPhysics(),
+                    itemCount: list.value.length,
+                    itemBuilder: (context, index) => Task(
+                      list.value[index],
+                      first: index == 0,
+                      last: index == list.value.length-1,
+                    ),
+                  ),
+                ),
+                if (list.value.isNotEmpty)
+                  const Divider(
+                    height: 0,
+                  ),
+                InkWell(
+                  borderRadius: BorderRadius.vertical(
+                      top: Radius.circular(
+                          controller.taskList.isEmpty ? 13 : 0),
+                      bottom: const Radius.circular(13)),
+                  onTap: () => taskCreatingDialog(context, scrollControl),
+                  child: ListTile(
+                      visualDensity: const VisualDensity(
+                          horizontal: VisualDensity.minimumDensity),
+                      contentPadding:
+                          const EdgeInsets.symmetric(horizontal: appPadding),
+                      leading: const Padding(
+                        padding: EdgeInsets.all(appPadding * 2),
+                        child: Icon(Icons.add),
                       ),
-                    ),
-                  ),
-                  if (list.value.isNotEmpty)
-                    const Divider(
-                      height: 0,
-                    ),
-                  InkWell(
-                    borderRadius: BorderRadius.vertical(
-                        top: Radius.circular(
-                            controller.taskList.isEmpty ? 13 : 0),
-                        bottom: const Radius.circular(13)),
-                    onTap: () => taskCreatingDialog(context, scrollControl),
-                    child: ListTile(
-                        visualDensity: const VisualDensity(
-                            horizontal: VisualDensity.minimumDensity),
-                        contentPadding:
-                            const EdgeInsets.symmetric(horizontal: appPadding),
-                        leading: const Padding(
-                          padding: EdgeInsets.all(appPadding * 2),
-                          child: Icon(Icons.add),
-                        ),
-                        title: Text(t.homepage.newtask)),
-                  ),
-                ],
-              );
-            }),
-          ),
+                      title: Text(t.homepage.newtask)),
+                ),
+              ],
+            );
+          }),
         ),
       ),
     );
